aaa97f57eb82748ef3287c24075307e2439247c1
Himanshu Mishra <himanshu404mishra@gmail.com>
2025-04-09 13:03:56 +0000

    khaini commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Home from "./pages/Home"
 
 const App = () => {
   return (
-    <div className="text-4xl">Hello World</div>
+    <>
+      <Home/>
+    </>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <Header>
+        <h1>Khaini Gang <sub>org.</sub></h1>
+        <nav>
+            <ul>
+                <li>About Us</li>
+                <li>Mission</li>
+                <li>Announcements</li>
+                <li>Notices</li>
+                <li>Contact Us</li>
+            </ul>
+        </nav>
+    </Header>
+  )
+}
+
+export default Header
